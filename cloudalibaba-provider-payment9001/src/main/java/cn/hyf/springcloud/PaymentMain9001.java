e6be83fd68b76d22579071103fee2b8a1739188b
lanranhuyoujie <1619987776@qq.com>
2020-04-14 09:17:22 +0800

    SpringCloud微服务架构

---
+++ cloudalibaba-provider-payment9001/src/main/java/cn/hyf/springcloud/PaymentMain9001.java
@@ -0,0 +1,16 @@
+package cn.hyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created By 黄一飞 On 2020 04 14 9:14
+ */
+@SpringBootApplication
+public class PaymentMain9001
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
